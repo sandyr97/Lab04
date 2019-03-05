dea6200de6dc3d10f58f1f9671a79f38f05bfdb2
Sandy Rech <rechs97@gmail.com>
2019-03-05 11:11:08 -0600

    added working questions 3-5

--- EX_2/quiz.php
+++ EX_2/quiz.php
@@ -9,7 +9,8 @@ $radioVal1 = $_POST["myRadio1"];
  else {
    echo "Question 1: What is 2+2? <br> Your answer: ".$radioVal1."<br> Correct answer: 4 <br>";
  }
-echo "<br>";
+ echo "<br>";
+
  $radioVal2 = $_POST["myRadio2"];
   if ($radioVal2=="10")
   {
@@ -19,6 +20,36 @@ echo "<br>";
   else {
     echo "Question 2: What is 20-10? <br> Your answer: ".$radioVal2."<br> Correct answer: 10 <br>";
   }
-
- echo "<br>Total Score: ".(($correct/2)*100). "%";
+  echo "<br>";
+  $radioVal3 = $_POST["myRadio3"];
+   if ($radioVal3=="50")
+   {
+     echo "Question 3: What is 25*2? <br> Your answer: ".$radioVal3."<br> Correct answer: 50 <br>";
+     $correct=$correct+1;
+   }
+   else {
+     echo "Question 3: What is 25*2? <br> Your answer: ".$radioVal3."<br> Correct answer: 50 <br>";
+   }
+   echo "<br>";
+   $radioVal4 = $_POST["myRadio4"];
+    if ($radioVal4=="7")
+    {
+      echo "Question 4: What is 35/5? <br> Your answer: ".$radioVal4."<br> Correct answer: 7 <br>";
+      $correct=$correct+1;
+    }
+    else {
+    echo "Question 4: What is 35/5? <br> Your answer: ".$radioVal4."<br> Correct answer: 7 <br>";
+    }
+    echo "<br>";
+    $radioVal5 = $_POST["myRadio5"];
+     if ($radioVal5=="64")
+     {
+       echo "Question 5: What is 8<sup>2</sup>? <br> Your answer: ".$radioVal5."<br> Correct answer: 64 <br>";
+       $correct=$correct+1;
+     }
+     else {
+     echo "Question 5: What is 8<sup>2</sup>? <br> Your answer: ".$radioVal5."<br> Correct answer: 64 <br>";
+     }
+     echo "<br>";
+ echo "<br>Total Score: ".(($correct/5)*100). "%";
 ?>
