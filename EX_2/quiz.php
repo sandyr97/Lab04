8fa0aaf4174ec02d274310390234c5eadf6fac52
Sandy Rech <rechs97@gmail.com>
2019-03-05 10:45:16 -0600

    added working question 2

--- EX_2/quiz.php
+++ EX_2/quiz.php
@@ -1,13 +1,24 @@
 <?php
 $correct=0;
-$radioVal = $_POST["myRadio"];
- if ($radioVal=="fourth")
+$radioVal1 = $_POST["myRadio1"];
+ if ($radioVal1=="4")
  {
-   echo "Question 1: What is 2+2? <br> Your answer: 4 <br> Correct answer: 4 <br>";
+    echo "Question 1: What is 2+2? <br> Your answer: ".$radioVal1."<br> Correct answer: 4 <br>";
    $correct=$correct+1;
  }
  else {
-   echo "Question 1: What is 2+2? <br> Your answer: ".$radioVal."<br> Correct answer: 4 <br>";
+   echo "Question 1: What is 2+2? <br> Your answer: ".$radioVal1."<br> Correct answer: 4 <br>";
  }
- echo "Total Score: ".(($correct/1)*100). "%";
+echo "<br>";
+ $radioVal2 = $_POST["myRadio2"];
+  if ($radioVal2=="10")
+  {
+    echo "Question 2: What is 20-10? <br> Your answer: ".$radioVal2."<br> Correct answer: 10 <br>";
+    $correct=$correct+1;
+  }
+  else {
+    echo "Question 2: What is 20-10? <br> Your answer: ".$radioVal2."<br> Correct answer: 10 <br>";
+  }
+
+ echo "<br>Total Score: ".(($correct/2)*100). "%";
 ?>
